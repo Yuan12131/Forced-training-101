f794610e45ad7e53b12d3c510cdc0d1b16c9c98f
leeyuan <dbdks32123@gmail.com>
2023-11-06 19:36:37 +0900

    문제 14 - javascript의 data-type

---
+++ Q14.js
@@ -0,0 +1,28 @@
+// ? 자바스크립트에서 사용하는 데이터 타입들을 나열
+
+// 숫자
+const a = 10;
+console.log(typeof(10))
+
+// 문자열
+const b = "10";
+console.log(typeof(b))
+
+// 객체
+const lee = {
+    name : "yuan",
+    age : 23
+}
+
+console.log(lee)
+
+// bollean
+const d = true;
+console.log(typeof(isStudent));
+
+// 변수
+e = 10202;
+let f = 20202;
+var g = 30202;
+const h = 40202;
+console.log(h)
