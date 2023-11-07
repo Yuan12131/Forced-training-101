8e2ac2e6fc0c70b67c1c1e048cdc9d52c51e90c2
leeyuan <dbdks32123@gmail.com>
2023-11-07 19:26:26 +0900

    arr_method_Q9 - reverse()

---
+++ arr_method_Q9.js
@@ -0,0 +1,13 @@
+// 배열의 순서를 반전시키는 메서드
+
+const array1 = ['one', 'two', 'three'];
+console.log('array1:', array1);
+// Expected output: "array1:" Array ["one", "two", "three"]
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed);
+// Expected output: "reversed:" Array ["three", "two", "one"]
+
+// Careful: reverse is destructive -- it changes the original array.
+console.log('array1:', array1);
+// Expected output: "array1:" Array ["three", "two", "one"]
