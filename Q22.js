f1028137767700a3b900ba8008382233373ece33
leeyuan <dbdks32123@gmail.com>
2023-11-07 18:57:07 +0900

    문제 22

---
+++ Q22.js
@@ -0,0 +1,17 @@
+// x와 y가 같은지, z가 0이 아닌지 확인하는 복합 조건식 작성
+
+const x = 1;
+const y = 2;
+const z = 0;
+
+if(x === y){
+  console.log("x와 y는 같습니다.")
+}
+if(z === 0){
+  console.log("x와 y는 같지 않습니다.")
+  console.log("z는 0입니다.")
+}
+else {
+  console.log("x와 y는 같지 않습니다.")
+  console.log("z는 0이 아닙니다.")
+}
