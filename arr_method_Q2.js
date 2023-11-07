ff432ee6cf4455f5cc380d21250a10a098ae556d
leeyuan <dbdks32123@gmail.com>
2023-11-07 19:08:27 +0900

    arr_method_Q2 - pop()

---
+++ arr_method_Q2.js
@@ -0,0 +1,9 @@
+// 배열에서 마지막 요소를 제거하고, 그 요소를 반환
+
+const fruits = ["apple", "banana", "cherry"];
+
+// 배열에서 마지막 요소 제거
+const lastFruit = fruits.pop();
+
+console.log(fruits);    // ["apple", "banana"]
+console.log(lastFruit); // "cherry" (제거된 요소)
