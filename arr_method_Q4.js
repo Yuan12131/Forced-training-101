b5648792d71215357202a34adda115d7f4f0e0ed
leeyuan <dbdks32123@gmail.com>
2023-11-07 19:11:11 +0900

    arr_method_Q4 - unshift()

---
+++ arr_method_Q4.js
@@ -0,0 +1,9 @@
+// 배열의 시작에 하나 이상의 요소를 추가하고, 배열의 새길이를 반환
+
+const array1 = [1, 2, 3];
+
+console.log(array1.unshift(4, 5));
+// Expected output: 5
+
+console.log(array1);
+// Expected output: Array [4, 5, 1, 2, 3]
