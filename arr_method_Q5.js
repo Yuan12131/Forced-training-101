19553ed903f1b033c3d09b7e832aafc6856e1f6c
leeyuan <dbdks32123@gmail.com>
2023-11-07 19:13:55 +0900

    arr_method_Q5 - slice()

---
+++ arr_method_Q5.js
@@ -0,0 +1,22 @@
+// 배열의 일부를 얕은 복사본으로 추출하고, 새로운 배열 객체로 반환
+
+//slice(begin, end) : begin 부터 end 까지(end 미포함)
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animals.slice(2));
+// Expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.slice(2, 4));
+// Expected output: Array ["camel", "duck"]
+
+console.log(animals.slice(1, 5));
+// Expected output: Array ["bison", "camel", "duck", "elephant"]
+
+console.log(animals.slice(-2));
+// Expected output: Array ["duck", "elephant"]
+
+console.log(animals.slice(2, -1));
+// Expected output: Array ["camel", "duck"]
+
+console.log(animals.slice());
+// Expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
