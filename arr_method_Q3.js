185824df075646444d5ebcec787bf55ee62cd1f1
leeyuan <dbdks32123@gmail.com>
2023-11-07 19:09:54 +0900

    arr_method_Q3 - shift()

---
+++ arr_method_Q3.js
@@ -0,0 +1,11 @@
+// 배열에서 첫 번째 요소를 제가하고, 그 요소를 반환
+
+const array1 = [1, 2, 3];
+
+const firstElement = array1.shift();
+
+console.log(array1);
+// Expected output: Array [2, 3]
+
+console.log(firstElement);
+// Expected output: 1
