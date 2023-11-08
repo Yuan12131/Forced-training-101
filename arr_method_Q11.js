84cf0a489fed5bd5c139b58ffc1d856138b3bafa
leeyuan <dbdks32123@gmail.com>
2023-11-08 19:17:27 +0900

    arr_method_Q11- indexof()

---
+++ arr_method_Q11.js
@@ -0,0 +1,9 @@
+// ? 배열에서 지정된 요소를 찾고 그 인덱스를 반환하는 메서드
+
+const classRoom = ['water', 'note', 'board', 'computer'];
+
+console.log(classRoom.indexOf('water'));
+// Expected output: 0
+
+console.log(classRoom.indexOf('phone'));
+// Expected output: -1
