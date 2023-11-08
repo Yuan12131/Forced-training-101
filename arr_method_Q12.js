64a42f15ff5dbdd1a45699933357846f00fc5874
leeyuan <dbdks32123@gmail.com>
2023-11-08 19:19:19 +0900

    arr_method_Q9 - lastIndexof()

---
+++ arr_method_Q12.js
@@ -0,0 +1,9 @@
+// ? 배열에서 지정된 요소를 뒤에서부터 찾고, 그 인덱스를 반환하는 메서드
+
+const classRoom = ['water', 'note', 'board', 'computer', 'water'];
+
+console.log(classRoom.lastIndexOf('water'));
+// Expected output: 0
+
+console.log(classRoom.lastIndexOf('phone'));
+// Expected output: -1
