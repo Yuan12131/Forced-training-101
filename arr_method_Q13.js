04f136019a8d4dc29d21854ff41c4b12e5a365ff
leeyuan <dbdks32123@gmail.com>
2023-11-08 19:21:21 +0900

    arr_method_Q12 - find()

---
+++ arr_method_Q13.js
@@ -0,0 +1,8 @@
+// ? 주어진 테스트 함수를 만족하는 배열의 첫 번째 요소의 값을 반환하는 메서드
+
+const array = [5, 12, 8, 130, 44];
+
+const found = array.find((element) => element > 10);
+
+console.log(found);
+// Expected output: 12
