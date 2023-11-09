976b5c686f177751ed66ddc5295f2aceb8acbf8b
leeyuan <dbdks32123@gmail.com>
2023-11-09 20:35:07 +0900

    arr_method_Q22 - includes()

---
+++ arr_method_Q22.js
@@ -0,0 +1,14 @@
+// ? 배열이 특정 요소를 포함하고 있는지 판별하는 메서드
+
+const array = [1, 2, 3];
+
+console.log(array.includes(2));
+// Expected output: true
+
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat'));
+// Expected output: true
+
+console.log(pets.includes('at'));
+// Expected output: false
