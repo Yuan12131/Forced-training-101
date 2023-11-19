1226c271fbe37174b1650d3228888e517bbe23e3
leeyuan <dbdks32123@gmail.com>
2023-11-19 17:40:39 +0900

    문제 77 - DOM 요소 선택

---
+++ Q77.js
@@ -0,0 +1,5 @@
+// ? DOM 요소를 선택하는 코드
+
+const elementById = document.getElementById('root');
+const elementBySelector = document.querySelector('.mySelector');
+const elementsBySelectorAll = document.querySelectorAll('.mySelector');
