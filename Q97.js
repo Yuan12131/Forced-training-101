87036e8507193aaeb55e1309794c7cc89523d7ad
leeyuan <dbdks32123@gmail.com>
2023-11-19 18:37:43 +0900

    문제 97 - 문자열 템플릿 리터럴

---
+++ Q97.js
@@ -0,0 +1,12 @@
+// ? Javascript에서 문자열 템플릿 리터럴을 사용하는 코드
+
+// 템플릿 리터럴은 백틱 (backtick) 문자 `를 사용하여 문자열을 감싸는 형식
+// 변수나 표현식을 ${} 안에 넣어서 문자열 중간에 삽입
+const name = 'lee';
+const age = 28;
+
+// 문자열 템플릿 리터럴 사용
+const greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+
+// 결과 출력
+console.log(greeting);
